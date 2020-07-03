3abc92abf8f9d4198058a18717bdd19634fb723f
Youssef Eddaif <63888397+yeddaif@users.noreply.github.com>
2020-07-03 17:19:26 +0100

    Add files via upload

---
+++ location.js
@@ -0,0 +1,13 @@
+$.ajax({
+    url: "https://geolocation-db.com/jsonp",
+    jsonpCallback: "callback",
+    dataType: "jsonp",
+    success: function(location) {
+        $('#country').html(location.country_name);
+        $('#state').html(location.state);
+        $('#city').html(location.city);
+        $('#latitude').html(location.latitude);
+        $('#longitude').html(location.longitude);
+        $('#ip').html(location.IPv4);
+    }
+});
